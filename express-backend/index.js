bd6c91adb5d4e0d5282561f7c1fc8d38e6210dfb
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2024-10-21 09:46:51 +0100

    added a request to del, patch and push

--- express-backend/index.js
+++ express-backend/index.js
@@ -90,6 +90,26 @@ app.get('/:id', (req, res) => {
   }
 });
 
+// POST request to create a new movie
+app.post('/', (req, res) => {
+  res.send("Create a new movie");
+  console.log(req.body);
+  
+});
+
+// PATCH request to update a movie
+app.patch('/movies/:id', (req, res) => {
+  res.send("Update/Edit an existing movie");
+  console.log(req.body);
+  
+});
+
+// DELETE request to delete a movie
+app.delete('/movies/:id', (req, res) => {
+  res.send("Delete a movie");
+  console.log(req.body);
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
